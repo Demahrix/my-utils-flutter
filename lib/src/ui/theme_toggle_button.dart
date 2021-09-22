9d13a4c843f51519ee3ec7da3898e0333f6c5b42
demahrix <hamed.zgr@gmail.com>
2021-09-22 01:13:51 +0000

    Ajout de la possibilite d'ajouter des unicodes

--- lib/src/ui/theme_toggle_button.dart
+++ lib/src/ui/theme_toggle_button.dart
@@ -3,18 +3,37 @@ import 'package:my_utils/src/services/theme_switcher_bloc.dart';
 
 class ThemeToggleButton extends StatelessWidget {
 
+  final Widget lightIcon;
+  final Widget darkIcon;
   final ThemeSwitcherBloc bloc;
-  ThemeToggleButton(this.bloc);
+
+  // 🌚🌜☀️🌞
+  ThemeToggleButton(
+    this.bloc, {
+      this.lightIcon: const Icon(Icons.wb_sunny_outlined, color: Colors.black),
+      this.darkIcon: const Icon(Icons.nights_stay_outlined, color: Colors.white)
+  });
+
+  ThemeToggleButton.unicode(this.bloc):
+    lightIcon = const Text(
+        "🌞",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 18.0,
+        ),
+    ), darkIcon = const Text(
+        "🌚",
+        style: TextStyle(
+          fontSize: 18.0
+        ),
+    );
 
   @override
   Widget build(BuildContext context) {
     final isLight = Theme.of(context).brightness == Brightness.light;
     return IconButton(
       onPressed: bloc.change,
-      icon: Icon(
-        isLight ? Icons.wb_sunny_outlined : Icons.nights_stay_outlined,
-        color: Theme.of(context).iconTheme.color
-      ),
+      icon: isLight ? lightIcon : darkIcon,
       tooltip: "Mode jour/sombre",
     );
   }
