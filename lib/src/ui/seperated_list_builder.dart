3147d6387d559805743e06a625c783c8674129f3
demahrix <hamed.zgr@gmail.com>
2022-10-13 19:03:35 +0000

    Fix bug

--- lib/src/ui/seperated_list_builder.dart
+++ lib/src/ui/seperated_list_builder.dart
@@ -4,7 +4,7 @@ List<Widget> separatedListBuilder(
   int length, {
     required Widget Function(int index) builder,
     required Widget Function(int index) separatedBuilder
-  }) => List<Widget>.generate(length * 2 - 1, (index) {
+  }) => List<Widget>.generate(length == 0 ? 0 : length * 2 - 1, (index) {
     if (index & 1 == 1)
       return separatedBuilder(index ~/ 2);
     return builder(index ~/ 2);
