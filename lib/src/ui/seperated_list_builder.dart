59fa9f581aed32d946b21d4e7a03ad7f4332940c
demahrix <hamed.zgr@gmail.com>
2023-10-21 13:24:04 +0000

    Add [separatedListBuilderAlt]

--- lib/src/ui/seperated_list_builder.dart
+++ lib/src/ui/seperated_list_builder.dart
@@ -9,3 +9,16 @@ List<Widget> separatedListBuilder(
       return separatedBuilder(index ~/ 2);
     return builder(index ~/ 2);
   }, growable: false);
+
+List<Widget> separatedListBuilderAlt<T>(
+  List<T> items, {
+    required Widget Function(T item, int index) builder,
+    required Widget Function(int index) separatedBuilder
+  }) {
+    var count = items.length;
+    return List<Widget>.generate(count == 0 ? 0 : count * 2 - 1, (index) {
+      if (index & 1 == 1)
+        return separatedBuilder(index ~/ 2);
+      return builder(items[index], index ~/ 2);
+    }, growable: false);
+  }
